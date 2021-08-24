dbc3a1e521278bde7c75ea712b898cbb645d4dca
Am Samra <amamsamra@gmail.com>
2021-08-24 20:13:57 +0100

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Amarjit Samra on 30/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
